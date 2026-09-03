dc5e38bf13e42f02d48d33e47b0ed2ef07fc2f36
Rodrigo de Oliveira <rodrigodotnet@gmail.com>
2026-09-03 09:29:45 -0300

    feat: define ADR validation codes

---
+++ src/AdrGuard/Validation/ValidationCodes.cs
@@ -0,0 +1,13 @@
+namespace AdrGuard.Validation;
+
+internal static class ValidationCodes
+{
+    internal const string InvalidFileName = "ADR001";
+    internal const string MissingTitle = "ADR002";
+    internal const string MissingStatus = "ADR003";
+    internal const string InvalidStatus = "ADR004";
+    internal const string MissingSection = "ADR005";
+    internal const string DuplicateId = "ADR006";
+    internal const string BrokenReference = "ADR007";
+    internal const string MissingSupersededBy = "ADR008";
+}
